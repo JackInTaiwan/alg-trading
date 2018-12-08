904448d797185590294de9afde22be706ed19c5f
jackintaiwan <jackconvolution@gmail.com>
2018-12-08 20:26:51 +0800

    init trainers/

---
+++ trainers/base_trainer.py
@@ -0,0 +1,35 @@
+import abc
+
+
+
+class BaseTrainer(metaclass=abc.ABCMeta):
+    def __init__(self, data=None):
+        self.__data = data
+
+
+    @staticmethod
+    @abc.abstractmethod
+    def add_param_parser(subparser):
+        raise NotImplementedError
+
+    @staticmethod
+    @abc.abstractmethod
+    def set_trainer_parameters(args):
+        raise NotImplementedError
+        
+
+    @abc.abstractmethod
+    def data_preprocess(self, data):
+        """
+        return: data (any type you want)
+        """
+        raise NotImplementedError
+
+
+    def load_data(self, data):
+        self.__data = data
+
+
+    @abc.abstractmethod
+    def train(self):
+        raise NotImplementedError
---
+++ trainers/simple_trainer.py
@@ -0,0 +1,28 @@
+from .base_trainer import BaseTrainer
+
+
+
+class SimpleTrainer(BaseTrainer):
+    def __init__(self, data=None, *args, **kwargs):
+        super(SimpleTrainer, self).__init__(*args, **kwargs)
+        self.__data = data
+
+
+    def add_param_parser(subparser):
+        subparser.add_argument("-a", type=int)
+
+
+    def set_trainer_parameters(args):
+        pass
+
+
+    def data_preprocess(self, data):
+        return data
+
+
+    def load_data(self, data):
+        self.__data = data
+
+
+    def train(self):
+        pass
